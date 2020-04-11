4d35bd638527377bca1b7035513a4a7709ce6f4e
miki takezawa <mikiaaamiki@yahoo.co.jp>
2020-04-11 23:37:46 +0900

    kadai

--- scoring.js
+++ scoring.js
@@ -1,22 +1,22 @@
 $(document).ready(function(){
   // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]の入力値を取得して合計点と平均点を出すロジックを作ります。
   function score_indicate(){
+    // 変数「subject_points」に
+    // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]の配列を代入します。
     let subject_points = [Number($('#national_language').val()),
                           Number($('#english').val()),
                           Number($('#mathematics').val()),
                           Number($('#science').val()),
                           Number($('#society').val())
                           ];
-    // 変数「subject_points」に
-    // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]の配列を代入します。
-
     // 変数「sum」に
     // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]をそれぞれ足します。
     // ヒント! 配列を一つづつ取り出して足していきます。
-     let sum = subject_points.reduce(function(accumulator, currentValue){
-       return accumulator + currentValue;
-     })
-
+    let sum = subject_points[0];
+    sum = sum + subject_points[1];
+    sum = sum + subject_points[2];
+    sum = sum + subject_points[3];
+    sum = sum + subject_points[4];
     // 「合計点：」(class="sum_indicate")に変数「sum」(合計点)を出力させます。
     $("#sum_indicate").text(sum);
     let average = sum / subject_points.length;
@@ -35,13 +35,11 @@ $(document).ready(function(){
     if ( averageIndicate >= 80){
       return "A";
       // もし「averageIndicate」が60以上なら"B"を返します。
+    } else if ( averageIndicate >= 60) {
+      return "B";
       // もし「averageIndicate」が40以上なら"C"を返します。
       // もし「averageIndicate」がそれ以外の点数なら"D"を返します。
-    } else if( averageIndicate >= 60){
-      return "B"
-      } else if(averageIndicate >= 40){
-        return "C";
-      } else {
+    } else {
       return "D";
     }
   };
@@ -53,8 +51,11 @@ $(document).ready(function(){
                           Number($('#science').val()),
                           Number($('#society').val())
                           ];
+    // 変数「number」に入力した教科の数を代入します。
     let number = subject_points.length;
     // 変数「judge」に"合格"を代入しておきます。
+    // 入力したそれぞれの教科の点数が60点よりも低いと変数「judge」に"不合格"を再代入して「judge」を返します。
+    // ヒント! 「javascript 点数 合格 不合格 ロジック」で検索してみてください。
     let judge = "合格";
     for(let i = 0; i < number;i++){
       if(subject_points[i] < 60){
@@ -73,12 +74,9 @@ $(document).ready(function(){
     let achievement = get_achievement();
     // 変数「pass_or_failure」に「get_pass_or_failure()の戻り値」を代入します。
     let pass_or_failure = get_pass_or_failure();
-    $('#btn-declaration').click(function(){
-      $("#alert-indicate").remove();
-
+    $("#alert-indicate").remove();
     // 「最終ジャッジ」(id="alert-indicate)ボタンを押したら「あなたの成績は${achievement}で${pass_or_failure}です」が出力される処理です。
     $('#declaration').append(`<label id="alert-indicate" class="alert alert-info">あなたの成績は${achievement}で${pass_or_failure}です</label>`);
-    });
   };
   // [国語の点数,英語の点数,数学の点数,理科の点数,社会の点数]のいずれかの点数が変更された際に「function score_indicate()」を発火させる処理です。
   $('#national_language, #english, #mathematics, #science, #society').change(function() {
@@ -89,15 +87,11 @@ $(document).ready(function(){
     $("#evaluation").text(get_achievement());
   });
   // 「判定」(class="btn-judge")ボタンを押したら「function et_pass_or_failure()」が出力される処理です。
-  $('#btn-judge').click(function(){
-    $('#judge').text(get_pass_or_failure());
+    $('#btn-judge').click(function() {
+    $("#judge").text(get_pass_or_failure());
   });
   // 「最終ジャッジ」(class="btn-declaration")ボタンを押したら「function judgement()」が出力される処理です。
-  //$('#btn-declaration').click(function() {
-    //$("#declaration").text(judgement());
-  //$('#btn-declaration').click(function(){
-    //$('#alert-indicate').remove();
-    $('#btn-declaration').click(function(){
-      $('#btn-declaration').text(judgement());
-    });
+  $('#btn-declaration').click(function() {
+    $('#declaration').text(judgement());
   });
+});
